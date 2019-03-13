91085a89bade848042f94d44576e3a7f6204e5b9
Vlad <novlad26@gmail.com>
2019-03-14 00:53:23 +0300

    Initialize repo

---
+++ public/js/builder.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function buildFormulaWithPrinting() {
+    var formula = document.getElementById("formula").value;
+    
+    buildFormula(formula);
+}
+
+function buildFormula(formula) {
+    var sknfFormula;
+    
+    return sknfFormula;
+}
+
+
